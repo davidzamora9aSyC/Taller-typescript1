eb2e187319a516af59d9a8057d8a9435c72a4ae8
d.zamorac <d.zamorac@uniandes.edu.co>
2022-10-21 12:53:52 -0500

    botones no funcionando

--- main.ts
+++ main.ts
@@ -4,11 +4,18 @@ import { dataSeries } from './dataSeries.js';
 
 let seriesTbody: HTMLElement = document.getElementById('series')!;
 let promedioTbody: HTMLElement = document.getElementById('promedio')!;
+let infoTbody: HTMLElement = document.getElementById('info')!;
+
 
 
 renderSeriesInTable(dataSeries);
 
 
+
+const handleClick =(name:string)=>{
+  console.log(name)
+  renderInfo(name)
+}
 
 function renderSeriesInTable(series: Serie[]): void {
   console.log('Desplegando series');
@@ -17,15 +24,15 @@ function renderSeriesInTable(series: Serie[]): void {
     
     trElement.innerHTML = `
                         <td>${serie.index}</td>
-                        <td>${serie.name}</td>
-                        <td>${serie.channel}</td>
+                        <td><div id='btn' class='p-0 m-0'> ${serie.name}</div></td>
+                        <td onClick={handleClick('nombre')}>${serie.channel}</td>
                         <td>${serie.seasons}</td>`;
     
     seriesTbody.appendChild(trElement);
     
   });
   let promedioCalculado = document.createElement("tr");
-  promedioCalculado.innerHTML= ` <div> ${getAverageOfSeasons(series)}</div>`;
+  promedioCalculado.innerHTML= ` <div> El promedio de temporadas es:&nbsp ${getAverageOfSeasons(series)}</div>`;
   promedioTbody.appendChild(promedioCalculado);
 }
 
@@ -41,3 +48,17 @@ function getAverageOfSeasons(series: Serie[]): number {
   return averageOfSeasons;
 }
 
+
+
+function renderInfo(serie:string):void {
+  let infoElement= document.createElement("div")
+  infoElement.innerHTML=`<div> ${serie} </div>
+                        <div> segundo </div>
+                          `
+  infoTbody.appendChild(infoElement)
+}
+let botonEscoger= document.getElementById("btn")!;
+botonEscoger.addEventListener('click', (e) =>{
+  handleClick('nombre')
+  console.log('click')
+})
--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,21 @@
 import { dataSeries } from './dataSeries.js';
 var seriesTbody = document.getElementById('series');
 var promedioTbody = document.getElementById('promedio');
+var infoTbody = document.getElementById('info');
 renderSeriesInTable(dataSeries);
+var handleClick = function (name) {
+    console.log(name);
+    renderInfo(name);
+};
 function renderSeriesInTable(series) {
     console.log('Desplegando series');
     series.forEach(function (serie) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "\n                        <td>".concat(serie.index, "</td>\n                        <td>").concat(serie.name, "</td>\n                        <td>").concat(serie.channel, "</td>\n                        <td>").concat(serie.seasons, "</td>");
+        trElement.innerHTML = "\n                        <td>".concat(serie.index, "</td>\n                        <td><div id='btn' class='p-0 m-0'> ").concat(serie.name, "</div></td>\n                        <td onClick={handleClick('nombre')}>").concat(serie.channel, "</td>\n                        <td>").concat(serie.seasons, "</td>");
         seriesTbody.appendChild(trElement);
     });
     var promedioCalculado = document.createElement("tr");
-    promedioCalculado.innerHTML = " <div> ".concat(getAverageOfSeasons(series), "</div>");
+    promedioCalculado.innerHTML = " <div> El promedio de temporadas es:&nbsp ".concat(getAverageOfSeasons(series), "</div>");
     promedioTbody.appendChild(promedioCalculado);
 }
 function getAverageOfSeasons(series) {
@@ -23,3 +28,13 @@ function getAverageOfSeasons(series) {
     var averageOfSeasons = totalSeasons / numberOfSeries;
     return averageOfSeasons;
 }
+function renderInfo(serie) {
+    var infoElement = document.createElement("div");
+    infoElement.innerHTML = "<div> ".concat(serie, " </div>\n                        <div> segundo </div>\n                          ");
+    infoTbody.appendChild(infoElement);
+}
+var botonEscoger = document.getElementById("btn");
+botonEscoger.addEventListener('click', function (e) {
+    handleClick('nombre');
+    console.log('click');
+});
